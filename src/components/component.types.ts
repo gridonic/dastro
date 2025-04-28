65a8b98cd0482947d348aec6dabcb0478d56b354
jfierz <julien@gridonic.ch>
2025-04-28 17:51:52 +0200

    feat: add more basic headless astro components

---
+++ src/components/component.types.ts
@@ -0,0 +1,25 @@
+export interface ImageAssetData {
+  responsiveImage?: {
+    __typename?: 'ResponsiveImage',
+    src: string,
+    width: number,
+    height: number
+  } | null
+}
+
+export interface VideoAssetData {
+  video?: {
+    __typename?: 'UploadVideoField',
+    streamingUrl: string,
+    muxPlaybackId: string,
+    duration?: number | null,
+    framerate?: number | null,
+    width: number,
+    height: number,
+    thumbnailUrl: string,
+    urlHighRes?: string | null,
+    urlMediumRes?: string | null,
+    urlLowRes?: string | null
+  } | null
+}
+
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
-import RecordLink from './RecordLink.astro';
+import RecordLink from './link/RecordLink.astro';
 
-export { RecordLink };
+import CommonStructuredText from './common/CommonStructuredText.astro';
+
+import ImageAsset from './media/ImageAsset.astro';
+import VideoAsset from './media/VideoAsset.astro';
+import MediaAsset from './media/MediaAsset.astro';
+
+export {RecordLink, CommonStructuredText, ImageAsset, VideoAsset, MediaAsset};
