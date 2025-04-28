4574260a72ab25008e66d811e39242eecfaf5a47
jfierz <julien@gridonic.ch>
2025-04-28 15:42:00 +0200

    feat: add record link component

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,9 @@
-import type {buildDastroContext, ExportTypesRouting} from "./src";
+import type {buildDastroContext, DastroTypes, ExportTypesRouting} from "./src";
 
 declare global {
   namespace App {
     interface Locals {
+      locale: DastroTypes['SiteLocale'];
       dastro: Omit<ReturnType<typeof buildDastroContext<DastroTypes>>, 'routing'> & { routing: () => ExportTypesRouting<DastroTypes> };
     }
   }
