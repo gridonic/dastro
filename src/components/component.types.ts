dae6ca4e383234f1dbcb656c6ac86ee03aec4849
jfierz <julien@gridonic.ch>
2025-09-24 14:38:24 +0200

    feat(module): add ids for each header and content module

--- src/components/component.types.ts
+++ src/components/component.types.ts
@@ -1,4 +1,4 @@
-import type {AstroBuiltinAttributes} from "astro";
+import type { AstroBuiltinAttributes } from 'astro';
 
 export type AstroComponent = (props: any) => any;
 
@@ -6,60 +6,72 @@ export type ClassValue = AstroBuiltinAttributes['class:list'];
 
 export interface Module {
   __typename: string;
+  id?: string;
 }
 
 export interface ImageAssetData {
   responsiveImage?: {
-    __typename?: 'ResponsiveImage',
-    src: string,
-    width: number,
-    height: number
-  } | null
+    __typename?: 'ResponsiveImage';
+    src: string;
+    width: number;
+    height: number;
+  } | null;
 }
 
 export interface VideoAssetData {
   video?: {
-    __typename?: 'UploadVideoField',
-    streamingUrl: string,
-    muxPlaybackId: string,
-    duration?: number | null,
-    framerate?: number | null,
-    width: number,
-    height: number,
-    thumbnailUrl: string,
-    urlHighRes?: string | null,
-    urlMediumRes?: string | null,
-    urlLowRes?: string | null
-  } | null
+    __typename?: 'UploadVideoField';
+    streamingUrl: string;
+    muxPlaybackId: string;
+    duration?: number | null;
+    framerate?: number | null;
+    width: number;
+    height: number;
+    thumbnailUrl: string;
+    urlHighRes?: string | null;
+    urlMediumRes?: string | null;
+    urlLowRes?: string | null;
+  } | null;
 }
 
 export interface VideoPlayerData {
-  __typename?: 'VideoRecord',
-    video: {
-    __typename: 'VideoExternalRecord',
-      videoReference: { __typename?: 'VideoField', provider: string, providerUid: string }
-  } | {
-    __typename: 'VideoInlineRecord',
-      showControls: boolean,
-      autoplay: boolean,
-      loop: boolean,
-      muted: boolean,
-      videoAsset: {
-      __typename?: 'VideoFileField',
-        video: { __typename?: 'UploadVideoField', width: number, height: number, mp4Url?: string | null }
-    }
-  },
+  __typename?: 'VideoRecord';
+  video:
+    | {
+        __typename: 'VideoExternalRecord';
+        videoReference: {
+          __typename?: 'VideoField';
+          provider: string;
+          providerUid: string;
+        };
+      }
+    | {
+        __typename: 'VideoInlineRecord';
+        showControls: boolean;
+        autoplay: boolean;
+        loop: boolean;
+        muted: boolean;
+        videoAsset: {
+          __typename?: 'VideoFileField';
+          video: {
+            __typename?: 'UploadVideoField';
+            width: number;
+            height: number;
+            mp4Url?: string | null;
+          };
+        };
+      };
   posterImage?: {
-    __typename?: 'FileField',
-    url: string,
-    alt?: string | null,
-    mimeType: string,
+    __typename?: 'FileField';
+    url: string;
+    alt?: string | null;
+    mimeType: string;
     responsiveImage?: {
-      __typename?: 'ResponsiveImage',
-      src: string,
-      width: number,
-      height: number,
-      sizes: string
-    } | null
-  } | null
+      __typename?: 'ResponsiveImage';
+      src: string;
+      width: number;
+      height: number;
+      sizes: string;
+    } | null;
+  } | null;
 }
